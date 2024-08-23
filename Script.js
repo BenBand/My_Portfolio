59f2a56b6f561a01be2ec1c4da7d48d128d72ebb
Bernard Banda <bandabernard917@gmail.com>
2024-08-23 16:48:42 +0200

    added changes to the navigaton part

--- Script.js
+++ Script.js
@@ -1,15 +1,18 @@
-function formValidation()v {
-    const userName = document.getElementById("name").Value;
+function formValidation() {
+    const userName = document.getElementById("name").value;
     const userEmail = document.getElementById("email").value;
     const userNumber = document.getElementById("namber").value;
+    const userMessage = document.getElementById("text_message").value;
 
     const errorName = document.getElementById("error-name");
     const errorEmail = document.getElementById("error-email");
     const erroNumber = document.getElementById("error-number");
+    const erroTextMessage = document.getElementById("error-text-message");
 
     errorName.textContent = "";
     errorEmail.textContent = "";
     erroNumber.textContent = "";
+    erroTextMessage.textContent = "";
 
     let isValid = true;
 
@@ -21,12 +24,18 @@ function formValidation()v {
         errorEmail.textContent = "Please enter your email";
         isValid = false;
     }
-    if(!userEmail.inclueds("@")){
+    if(!userEmail.includes("@")){
         errorEmail.textContent = "Please enter your email properly";
         isValid = false;
     }
-    if(userNumber === ""){
-        erroNumber.textContent = "Please enter your phone number";
+    
+    if(userNumber.length < 10 || userNumber.length > 13 ){
+        erroNumber.textContent = "Please enter your correct phone number";
+        isValid = false;
+    }
+    if(userMessage === ""){
+        erroTextMessage.textContent = "Text field cannot be empty";
+        isValid = false;
     }
     return isValid;
 }
